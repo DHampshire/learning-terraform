871a8a969b210cf53a17caf5df50f8d252afc430
DHampshire <160472103+DHampshire@users.noreply.github.com>
2024-04-19 15:14:02 +0100

    test

--- main.tf
+++ main.tf
@@ -14,6 +14,11 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.7.1"
+}
+
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
